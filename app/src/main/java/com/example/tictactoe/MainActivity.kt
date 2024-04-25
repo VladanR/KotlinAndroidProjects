a2d6948b740ba2e920385914b37d67dce5684e24
VladanR <x3m.rocker.035@gmail.com>
2024-04-25 12:49:13 +0200

    tic tac toe project

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,206 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import android.text.BoringLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.magnifier
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tictactoe.ui.theme.TicTacToeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TicTacToeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    TTTScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TTTScreen() {
+    // true = players turn  false = AI turn
+    val playerTurn = remember { mutableStateOf(true) }
+    val moves = remember {
+        mutableStateListOf<Boolean?>(
+            true, null, false, true, false, null, null, null, null
+        )
+    }
+
+    val onTap: (Offset) -> Unit = {
+        if (playerTurn.value) {
+            val x = (it.x/333).toInt()
+            val y = (it.y/333).toInt()
+            val positionInMoves = y * 3 + x
+            if (moves[positionInMoves] == null) {
+                moves[positionInMoves] = true
+                playerTurn.value = false
+            }
+        }
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Tic Tac Toe", fontSize = 30.sp, modifier = Modifier.padding(16.dp))
+        Header(playerTurn.value)
+        Board(moves, onTap)
+    }
+
+}
+
+@Composable
+fun Header(playerTurn: Boolean) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        val playerBoxColor = if (playerTurn) Color.Blue else Color.LightGray
+        val aitBoxColor = if (!playerTurn) Color.Red else Color.LightGray
+
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .background(playerBoxColor)
+        ) {
+            Text(
+                text = "Player", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+
+        Spacer(modifier = Modifier.width(50.dp))
+
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .background(aitBoxColor)
+        ) {
+            Text(
+                text = "Computer", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Composable
+fun Board(moves: List<Boolean?>, onTap: (Offset) -> Unit) {
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .padding(32.dp)
+            .background(Color.LightGray)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = onTap)
+            }
+    ) {
+        Column(verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxSize(1f)) {
+            Row(
+                modifier = Modifier
+                    .height(2.dp)
+                    .fillMaxSize(1f)
+                    .background(Color.Black)
+            ) {
+
+            }
+            Row(
+                modifier = Modifier
+                    .height(2.dp)
+                    .fillMaxSize(1f)
+                    .background(Color.Black)
+            ) {
+
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxSize(1f)
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(2.dp)
+                    .fillMaxSize(1f)
+                    .background(Color.Black)
+            ) { }
+            Column(
+                modifier = Modifier
+                    .width(2.dp)
+                    .fillMaxSize(1f)
+                    .background(Color.Black)
+            ) { }
+        }
+        Column(modifier = Modifier.fillMaxSize(1f)) {
+            for (i in 0..2) {
+                Row(modifier = Modifier.weight(1f)) {
+                    for (j in 0..2) {
+                        Column(modifier = Modifier.weight(1f)) {
+                            getComposableFromMove(move = moves[ i * 3 + j])
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun getComposableFromMove(move: Boolean?) {
+    when (move) {
+        true -> Image(
+            painter = painterResource(id = R.drawable.ic_x),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(1f),
+            colorFilter = ColorFilter.tint(Color.Blue)
+        )
+
+        false -> Image(
+            painter = painterResource(id = R.drawable.ic_o),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(1f),
+            colorFilter = ColorFilter.tint(Color.Red)
+        )
+
+        null -> Image(
+            painter = painterResource(id = R.drawable.ic_null),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(1f)
+        )
+    }
+}
